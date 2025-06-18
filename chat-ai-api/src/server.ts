e2185e969c6b27f499dcd1c4e8aa4e08f10a245b
Алексей Бельченков <u608110@gmail.com>
2025-06-18 12:52:47 +0400

    First endpoint

--- chat-ai-api/src/server.ts
+++ chat-ai-api/src/server.ts
@@ -11,6 +11,22 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Register user with Stream Chat
+// @ts-ignore
+app.post("/register-user", async (req, res): Promise<any> => {
+	const { name, email } = req.body;
+
+	if (!name || !email) {
+		res.status(400).json({
+			error: "Name and email are required!",
+		});
+	}
+
+	res.status(200).json({
+		message: "Successfully registered user!",
+	});
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
