261605d2ad75eb9cc1a0323afdfbc17b13fc898f
Алексей Бельченков <u608110@gmail.com>
2025-07-03 13:50:17 +0400

    Init pinia

--- chat-ai-ui/src/main.ts
+++ chat-ai-ui/src/main.ts
@@ -1,9 +1,17 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import { createPinia } from "pinia";
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate';
 
 import './style.css'
 import App from './App.vue'
 import { router } from "./router";
 
+// Init pinia
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedState);
+
+// Init App
 const app = createApp(App);
 app.use(router);
+app.use(pinia);
 app.mount('#app')
