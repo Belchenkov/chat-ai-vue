7617145acbde77b45bf3a4cfc44d764bb25d3779
Алексей Бельченков <u608110@gmail.com>
2025-07-06 09:13:47 +0400

    Make header component

---
+++ chat-ai-ui/src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import robotImage from '../assets/robot.png';
+</script>
+
+<template>
+  <div class="py-4 px-6 bg-gray-800 shadow-md flex justify-between items-center">
+    <img
+        :src="robotImage"
+        alt="Chat AI"
+        class="w-8 h-8"
+    />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
