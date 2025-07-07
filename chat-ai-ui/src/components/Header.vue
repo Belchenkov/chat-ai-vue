28258f6b169b5f08bf447f14c347a0aaf78cffe1
Алексей Бельченков <u608110@gmail.com>
2025-07-07 10:00:41 +0400

    Leave Chat/logout

--- chat-ai-ui/src/components/Header.vue
+++ chat-ai-ui/src/components/Header.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-  import robotImage from '../assets/robot.png';
-</script>
-
 <template>
   <div class="py-4 px-6 bg-gray-800 shadow-md flex justify-between items-center">
     <img
@@ -9,8 +5,28 @@
         alt="Chat AI"
         class="w-8 h-8"
     />
+    <h1 class="text-lg font-semibold">Chat AI</h1>
+    <button
+        class="text-gray-400 hover:text-white"
+        @click="logout"
+    >Logout</button>
   </div>
 </template>
+
+<script setup lang="ts">
+  import { useRouter } from "vue-router";
+
+  import robotImage from '../assets/robot.png';
+  import { useUserStore } from "../stores/user.ts";
+
+  const userStore = useUserStore();
+  const router = useRouter();
+
+  const logout = () => {
+    userStore.logout();
+    router.push("/");
+  };
+</script>
 
 <style scoped>
 
--- chat-ai-ui/src/views/ChatView.vue
+++ chat-ai-ui/src/views/ChatView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+  import Header from "../components/Header.vue";
 </script>
 
 <template>
-  <h1>Chat Page</h1>
+  <div class="flex flex-col h-screen bg-gray-900 test-white">
+    <Header />
+  </div>
 </template>
 
 <style scoped>
